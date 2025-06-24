a19e20e2eb42291e06f1182fbc4ac925bbada4d9
Adolfo <adolfoklk0307@gmail.com>
2025-06-24 01:48:16 -0400

    add video playback functionality with controls and integrate intro video

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,75 @@
-import { Text, View } from 'react-native'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import { useVideoPlayer, VideoView } from 'expo-video'
+import { useEffect, useState } from 'react'
+import { TouchableOpacity, View } from 'react-native'
 import '../global.css'
 
+const videoId = require('../assets/videos/intro.mp4')
+
 export default function Index() {
+  const [status, setStatus] = useState<'playing' | 'paused' | 'ended'>(
+    'playing'
+  )
+  const player = useVideoPlayer(videoId, (player) => {
+    player.play()
+    setStatus('playing')
+  })
+
+  useEffect(() => {
+    const onEnded = () => setStatus('ended')
+    player.addListener?.('playToEnd', onEnded)
+    return () => player.removeListener?.('playToEnd', onEnded)
+  }, [player])
+
+  let iconType: 'motion-play-outline' | 'motion-pause-outline' | 'replay' =
+    'motion-pause-outline'
+  if (status === 'paused') iconType = 'motion-play-outline'
+  if (status === 'ended') iconType = 'replay'
+
+  const icon = (
+    <MaterialCommunityIcons
+      name={iconType}
+      size={64}
+      color='#FF8C00'
+    />
+  )
+
+  const handleButtonPress = () => {
+    if (status === 'ended') {
+      player.currentTime = 0
+      player.play()
+      setStatus('playing')
+    } else if (status === 'playing') {
+      player.pause()
+      setStatus('paused')
+    } else {
+      player.play()
+      setStatus('playing')
+    }
+  }
+
   return (
-    <View className='flex-1 items-center justify-center p-6'>
-      <Text className='text-3xl font-extrabold text-op-red mb-2'>
-        ¡Bienvenido a la app de One Piece! 🏴‍☠️
-      </Text>
-      <Text className='text-2xl text-op-white mb-4 text-center'>
-        Explora el mundo de los piratas, descubre personajes, tripulaciones y
-        aventuras inspiradas en el universo de One Piece. ¡Zarpa hacia Grand
-        Line!
-      </Text>
+    <View className='flex-1 items-center justify-center bg-gray-900'>
+      <VideoView
+        style={{
+          height: '50%',
+          aspectRatio: 16 / 9,
+          borderRadius: 16
+        }}
+        player={player}
+        allowsFullscreen={false}
+        allowsPictureInPicture
+        allowsVideoFrameAnalysis={false}
+        nativeControls={false}
+      />
+      <View className='flex-row items-center justify-center mt-4'>
+        <TouchableOpacity
+          className='p-4 rounded-lg'
+          onPress={handleButtonPress}
+        >
+          {icon}
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
