7a197607e8f8b7c190ff998652577d0fe7c29c6f
Adolfo <adolfoklk0307@gmail.com>
2025-06-25 01:07:25 -0400

    remove unused Momentos and Personajes components

--- app/momentos.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export default function Momentos() {
-  return (
-    <View>
-      <Text>Momentos de mi vida</Text>
-    </View>
-  )
-}
--- app/personajes.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export default function Personajes() {
-  return (
-    <View>
-      <Text>Personajes de la aplicación</Text>
-    </View>
-  )
-}
