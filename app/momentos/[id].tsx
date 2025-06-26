6eed35a629568830afd9faef45757c7a703cc18d
Adolfo <adolfoklk0307@gmail.com>
2025-06-25 23:00:48 -0400

    refactor Momento and Momentos components to enhance video playback and display details

--- app/momentos/[id].tsx
+++ app/momentos/[id].tsx
@@ -1,9 +1,55 @@
-import { Text, View } from 'react-native'
+import { MOMENTOS } from '@/constants/momentos'
+import { useFocusEffect, useLocalSearchParams } from 'expo-router'
+import { useVideoPlayer, VideoView } from 'expo-video'
+import { useCallback } from 'react'
+import { ScrollView, Text, View } from 'react-native'
 
 export default function Momento() {
+  const { id } = useLocalSearchParams()
+  const momento = MOMENTOS.find((m) => m.id === id)
+
+  if (!momento) {
+    return (
+      <View className='flex-1 items-center justify-center'>
+        <Text className='text-white text-2xl'>Momento no encontrado</Text>
+      </View>
+    )
+  }
+  const player = useVideoPlayer(momento.uri, (player) => {
+    player.play()
+  })
+  useFocusEffect(
+    useCallback(() => {
+      player.play?.()
+      return () => {
+        player.pause?.()
+      }
+    }, [player])
+  )
+
   return (
-    <View>
-      <Text>Momento</Text>
-    </View>
+    <ScrollView className='flex-1'>
+      <View className='items-center p-4'>
+        <VideoView
+          style={{
+            width: '100%',
+            aspectRatio: 16 / 9,
+            borderRadius: 16,
+            backgroundColor: '#000'
+          }}
+          player={player}
+          allowsFullscreen
+          allowsPictureInPicture
+          allowsVideoFrameAnalysis={false}
+          nativeControls
+        />
+      </View>
+      <View className='px-4 mt-4 gap-2'>
+        <Text className='text-3xl text-neutral-100 font-bold'>
+          Descripción y contexto
+        </Text>
+        <Text className='text-xl text-neutral-200'>{momento.description}</Text>
+      </View>
+    </ScrollView>
   )
 }
--- app/momentos/index.tsx
+++ app/momentos/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native'
+import { MOMENTOS } from '@/constants/momentos'
+import { Link } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 export default function Momentos() {
   return (
-    <View>
-      <Text>Mis momentos favoritos</Text>
+    <View className='flex-1 items-center gap-8 px-4 pt-8'>
+      {MOMENTOS.map((momento) => (
+        <Link
+          key={momento.id}
+          asChild
+          href={{
+            pathname: '/momentos/[id]',
+            params: { id: momento.id }
+          }}
+        >
+          <TouchableOpacity className='w-full bg-neutral-800 rounded-2xl px-4 py-6 flex-row items-center gap-4 border border-orange-400'>
+            <View className='flex-1'>
+              <Text className='text-2xl font-bold text-white'>
+                {momento.title}
+              </Text>
+              <Text className='text-neutral-300 mt-1'>Ver más</Text>
+            </View>
+          </TouchableOpacity>
+        </Link>
+      ))}
     </View>
   )
 }
