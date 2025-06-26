62f2b2d0a33a9ba12ab0a5b78b7a54d485e2397d
Adolfo <adolfoklk0307@gmail.com>
2025-06-25 23:00:31 -0400

    add MOMENTOS constant with video details and descriptions

---
+++ constants/momentos.ts
@@ -0,0 +1,29 @@
+interface Momento {
+  id: string
+  title: string
+  description: string
+  uri: string
+}
+export const MOMENTOS: Momento[] = [
+  {
+    id: '1',
+    title: '¡EL ONE PIECE EXISTE!',
+    uri: 'https://res.cloudinary.com/dnmnyien6/video/upload/v1750872973/lzl8nnxhahh1qnilllcv.mp4',
+    description:
+      'En la etapa final de la gran guerra de Marineford, Barbablanca tiene un último encuentro contra Barbanegra, donde luego de notar las posibilidades de morir, Barbanegra le dice a su tripulación que disparen a Barbablanca.\n\nLuego de recibir una lluvia de balas, Barbablanca aún en pie, recuerda una conversación que tuvo con Gol D. Roger sobre lo que era la voluntad de la «D.» y reclama que Marshall D. Teach (Barbanegra) no es una persona con esa voluntad, para luego dar un mensaje a la marina y al mundo, diciendo que aparecerá una persona con la voluntad que decía Roger y encontrara ese gran tesoro, gritando:\n\n¡EL ONE PIECE EXISTE!'
+  },
+  {
+    id: '2',
+    title: '¡Raizo-dono está bien!',
+    uri: 'https://res.cloudinary.com/dnmnyien6/video/upload/v1750872971/g17onvb5py2jhweprdvm.mp4',
+    description:
+      "El país de Zou, donde habitan los minks, había sido asaltado por Jack, un miembro de los piratas de Kaido, quien estaba buscando a Raizo, un ninja del clan Kozuki que se decía que estaba en Zou.\n\nCon motivos de buscar a Raizo, Jack mantuvo una pelea contra los dos líderes de Zou, Inuarashi (Rey Perro) y Nekomamushi (Rey Gato) quienes se turnaban entre día y noche para pelear contra Jack.\n\nYa en el quinto día, Jack usó un arma de gas venenoso para adjudicarse la victoria. Tras esto, él y su tripulación interrogaron y torturaron a los minks, para conocer la localización de Raizo. Jack llegó tan lejos como para amputar la pierna izquierda y el brazo izquierdo de Inuarashi y Nekomamushi, respectivamente. Por motivos de ayudar a un compañero, Jack se retiró de Zou sin encontrar a Raizo.\n\nLuego de la llegada de los Mugiwara a Zou, Kin'emon se presenta ante Inuarashi y Nekomamushi, y luego pregunta por Raizo, quien se encontraba escondido en Zou. Inuarashi y Nekomamushi le dicen a Kin'emon que Raizo estaba bien, y que lo habían estado protegiendo todo este tiempo."
+  },
+  {
+    id: '3',
+    title: '¡Quiero vivir, llevenme al mar con ustedes!',
+    uri: 'https://res.cloudinary.com/dnmnyien6/video/upload/v1750900724/i-want-to-live_efpn2z.mp4',
+    description:
+      'Nico Robin, una de las nakamas de los Mugiwara, tiene una habilidad que está prohibida por el gobierno mundial, la habilidad de leer los Poneglyphs, unas piedras que revelan partes de la historia, contienen instrucciones sobre dónde encontrar otros Poneglyphs y/o las Armas Ancestrales y, en el caso de los Road Poneglyph, indican la ruta hacia Laugh Tale.\n\nEn el arco de Enies Lobby, Robin misma se entrega al gobierno mundial, ya que este la buscaba desde que era una niña y no quería que sus nakamas se metieran en problemas por ella, pero a pesar de eso, Luffy y los demás se infiltran en Enies Lobby para rescatarla. Ya frente a la Torre judicial, Robin aun le insiste a Luffy que no la rescate porque su vida está maldita y solo traerá desgracia a sus nakamas y Spandam, el jefe de CP9, se ríe de ella y señala una bandera del gobierno mundial, diciendo que las personas que están detrás de Robin es muy peligrosa.\n\nLuffy ante esto mencionó que ya sabe quien es el verdadero enemigo, le pide a Sogeking que destruya la bandera del gobierno mundial, dejando en claro que no le importa quien sea el enemigo y luego le grita a Robin que quiere escucharla diciendo que quiere vivir, a lo que Robin responde con un grito de desesperación y esperanza diciendo:\n\n"¡Quiero vivir, llévenme al mar con ustedes!"'
+  }
+]
